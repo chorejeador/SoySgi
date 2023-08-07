e42cb15db15fcced34136ade2a95fc9f96d0a6dc
ahernandez <iris2@softcode.es>
2023-08-07 12:03:47 -0600

    ajustes css y permisos de documentos, agregado de htaccess

--- application/views/gerentes/documentosView.php
+++ application/views/gerentes/documentosView.php
@@ -84,12 +84,12 @@
             }
 
             echo '<tr style="border-bottom: 1px solid #dcd9d9;">
-                <td><img style="height:50px;" src="'.base_url().'assets/img/documentos/'.$img.'"></td>
+                <td><img style="height:35px;" src="'.base_url().'assets/img/documentos/'.$img.'"></td>
                 <td>'.$key["Descripcion"].'</td>
                 <td>'.$key["FechaCrea"].'</td>
                 <td>
                     <a style="color: black;  border-color: black;" href="'.base_url('index.php/downloadFile/').$key["IdDocumento"].'/gestion" target="_blank" class="btn btn-outline-theme-3 icon-button edit-button text-black"><i class="simple-icon-arrow-down-circle"></i></a> 
-                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top" style="color: black; border-color: black;" href="'.base_url('/uploads/').$key["Url"].'.'.$key["Tipo"].'" target="_blank"  class="btn btn-outline-theme-3 icon-button view-button"><i class="simple-icon-eye"></i></a>
+                    <!--<a data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top" style="color: black; border-color: black;" href="'.base_url('/uploads/').$key["Url"].'.'.$key["Tipo"].'" target="_blank"  class="btn btn-outline-theme-3 icon-button view-button"><i class="simple-icon-eye"></i></a>-->
                   </td>
             </tr>';
          }
--- application/views/header/header.php
+++ application/views/header/header.php
@@ -71,27 +71,13 @@ header('Access-Control-Allow-Origin: *');
 </head>
 
 <body id="app-container" class="menu-default show-spinner">
-    <nav class="navbar fixed-top" style="background-color:white;">
-        <div class="d-flex align-items-center navbar-left"><a href="#" class="menu-button d-none d-md-block"><svg
-                    class="main" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 17">
-                    <rect x="0.48" y="0.5" width="7" height="1" />
-                    <rect x="0.48" y="7.5" width="7" height="1" />
-                    <rect x="0.48" y="15.5" width="7" height="1" />
-                </svg> <svg class="sub" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 17">
-                    <rect x="1.56" y="0.5" width="16" height="1" />
-                    <rect x="1.56" y="7.5" width="16" height="1" />
-                    <rect x="1.56" y="15.5" width="16" height="1" />
-                </svg> </a><a href="#" class="menu-button-mobile d-xs-block d-sm-block d-md-none"><svg
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 17">
-                    <rect x="0.5" y="0.5" width="25" height="1" />
-                    <rect x="0.5" y="7.5" width="25" height="1" />
-                    <rect x="0.5" y="15.5" width="25" height="1" />
-                </svg></a>
+    <nav class="navbar fixed-top" style="background-color:#C41818!important;">
+        <div class="d-flex align-items-center navbar-left"><a href="#" class="menu-button d-none d-md-block">                
             <!--<div class="search" data-search-path="Pages.Search03d2.html?q="><input placeholder="Search..."> <span
                     class="search-icon"><i class="simple-icon-magnifier"></i></span></div><a
                 class="btn btn-sm btn-outline-primary ml-3 d-none d-md-inline-block"
                 href="https://1.envato.market/5kAb">&nbsp;BUY&nbsp;</a>-->
-        </div><a class="navbar-logo" href="Dashboard.Default.html"><span class="logo d-none d-xs-block"></span> <span
+        </div><a class="navbar-logo" href="<?php echo base_url('index.php') ?>"><span class="logo d-none d-xs-block"></span> <span
                 class="logo-mobile d-block d-xs-none"></span></a>
         <div class="navbar-right">
             <div class="header-icons d-inline-block align-middle">
@@ -161,8 +147,9 @@ header('Access-Control-Allow-Origin: *');
             </div>
             <div class="user d-inline-block">
                 <button class="btn btn-empty p-0" type="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false"><span class="name"><?php echo $this->session->userdata("Name")?></span> 
-                    <span><img alt="Profile Picture" src="<?php echo base_url()?>assets/img/profiles/l-1.jpg"></span>
+                    aria-haspopup="true" aria-expanded="false">
+                    <span class="name" style="color:white;"><?php echo $this->session->userdata("Name")?></span> 
+                    <span><img alt="Profile Picture" src="<?php echo base_url()?>assets/img/notifications/1.jpg"></span>
                 </button>
                 <div class="dropdown-menu dropdown-menu-right mt-3">
                         <a href="<?php echo base_url('index.php/procesos'); ?>" class="dropdown-item">Administración</a>
