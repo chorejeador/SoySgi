a20ecd4e0818580826fae666c5f8b1aafd225514
Eduardo Urbina <deveduardo1@gmail.com>
2026-01-20 11:15:01 -0600

    Refactor: Cleaned up and standardized HTML structure in `misionpolitica.php` for improved readability and maintainability. Updated policy section content.
    Took 5 minutes

--- application/views/landing/misionpolitica.php
+++ application/views/landing/misionpolitica.php
@@ -1,137 +1,163 @@
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Portal SGI</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="<?php echo base_url()?>assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        
-        <link href="<?php echo base_url() ?>assets/css/roboto.css" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="<?php echo base_url()?>assets/css/styles.css" rel="stylesheet" />
-        <link href="<?php echo base_url()?>assets/css/custom_landing.css" rel="stylesheet" />        
-        <link rel="stylesheet" href="<?php echo base_url()?>assets/font/iconsmind-s/css/iconsminds.css">
-        <link rel="stylesheet" href="<?php echo base_url()?>assets/font/simple-line-icons/css/simple-line-icons.css">
-        <!--<link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <!--<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">-->-->
-        <link href="<?php echo base_url()?>assets/css/custom_carousel.css" rel="stylesheet" />
-    </head>
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<meta name="description" content=""/>
+	<meta name="author" content=""/>
+	<title>Portal SGI</title>
+	<!-- Favicon-->
+	<link rel="icon" type="image/x-icon" href="<?php echo base_url() ?>assets/favicon.ico"/>
+	<!-- Font Awesome icons (free version)-->
+	<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+	<!-- Google fonts-->
 
-    <body id="page-top">
-        <!-- lateral menu-->
-        <div class="card setting-toggle" href="#settings-offcanvas" data-bs-toggle="offcanvas">
-          <div class="card-body d-flex align-items-center py-md-2 px-2 py-1">
-            
-            <a href="https://www.facebook.com/delmornic/"  target="_blank"><img src="<?php echo base_url() ?>assets/img/facebook.png" width="26" alt=""></a>
-            <a href="https://www.instagram.com/delmornic/?hl=es-la" target="_blank"><img src="<?php echo base_url() ?>assets/img/instagram.png" width="26" alt=""></a>
-            <a href="https://www.youtube.com/@delmornicaragua932/featured" target="_blank"><img src="<?php echo base_url() ?>assets/img/youtube.png" width="26" alt=""></a>
-          </div>
-        </div>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-            <div class="container">
-                
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars ms-1"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0 ">
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url("index.php"); ?>">
-                                <img src="<?php echo base_url() ?>assets/img/logo.png" width="100" alt="">
-                            </a>
-                        </li>
+	<link href="<?php echo base_url() ?>assets/css/roboto.css" rel="stylesheet" type="text/css"/>
+	<!-- Core theme CSS (includes Bootstrap)-->
+	<link href="<?php echo base_url() ?>assets/css/styles.css" rel="stylesheet"/>
+	<link href="<?php echo base_url() ?>assets/css/custom_landing.css" rel="stylesheet"/>
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/font/iconsmind-s/css/iconsminds.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/font/simple-line-icons/css/simple-line-icons.css">
+	<!--<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">-->
+	<link href="<?php echo base_url() ?>assets/css/custom_carousel.css" rel="stylesheet"/>
+</head>
 
-                        <li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php'); ?>">INICIO</a></li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" data-mdb-toggle="dropdown" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            CORPORACIÓN DELMOR
-                          </a>
-                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="<?php echo base_url('index.php/misionPolitica') ?>">Misión y política</a></li>
-                            <li><a class="dropdown-item" href="<?php echo base_url('index.php/vision') ?>">Visión</a></li>
-                            <li><a class="dropdown-item" href="<?php echo base_url('index.php/principiosValores') ?>">Principios y valores</a></li>
-                          </ul>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/quienessomos');?>">¿QUIÉNES SOMOS?</a></li>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/portafolio'); ?>">PORTAFOLIO</a></li>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/trayectoria'); ?>">TRAYECTORIA</a></li>
-                        <?php 
-                        if($this->session->userdata("logged") == 1){
-                            echo '<li style="background-color: #ffffff5c;border-radius: 18px;" class="nav-item"><a class="nav-link" href="'.base_url('index.php/gerentesView').'">SISTEMA DE GESTIÓN INTEGRAL</a></li>';
-                        }
-                        ?>                        
-                        <li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/noticiaseventos'); ?>">NOTICIAS Y EVENTOS</a></li>
-                        <!--<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/instalaciones') ?>">INSTALACIONES</a></li>-->
-                        <li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/contactenos') ?>">CONTÁCTENOS</a></li>                       
-                        <?php
-                                                        
-                            if($this->session->userdata("logged") == 1){
-                                echo '<li class="nav-item">
-                                    <a class="p-1 nav-link btn  btn-sm text-uppercase" href="'.base_url('index.php/salir').'"> Cerrar Sesión</a>
+<body id="page-top">
+<!-- lateral menu-->
+<div class="card setting-toggle" href="#settings-offcanvas" data-bs-toggle="offcanvas">
+	<div class="card-body d-flex align-items-center py-md-2 px-2 py-1">
+
+		<a href="https://www.facebook.com/delmornic/" target="_blank"><img
+				src="<?php echo base_url() ?>assets/img/facebook.png" width="26" alt=""></a>
+		<a href="https://www.instagram.com/delmornic/?hl=es-la" target="_blank"><img
+				src="<?php echo base_url() ?>assets/img/instagram.png" width="26" alt=""></a>
+		<a href="https://www.youtube.com/@delmornicaragua932/featured" target="_blank"><img
+				src="<?php echo base_url() ?>assets/img/youtube.png" width="26" alt=""></a>
+	</div>
+</div>
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+	<div class="container">
+
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+				aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			Menu
+			<i class="fas fa-bars ms-1"></i>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0 ">
+				<li class="nav-item">
+					<a class="nav-link" href="<?php echo base_url("index.php"); ?>">
+						<img src="<?php echo base_url() ?>assets/img/logo.png" width="100" alt="">
+					</a>
+				</li>
+
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php'); ?>">INICIO</a></li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" data-mdb-toggle="dropdown" href="#" id="navbarDropdown"
+					   role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						CORPORACIÓN DELMOR
+					</a>
+					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<li><a class="dropdown-item" href="<?php echo base_url('index.php/misionPolitica') ?>">Misión y
+								política</a></li>
+						<li><a class="dropdown-item" href="<?php echo base_url('index.php/vision') ?>">Visión</a></li>
+						<li><a class="dropdown-item" href="<?php echo base_url('index.php/principiosValores') ?>">Principios
+								y valores</a></li>
+					</ul>
+				</li>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/quienessomos'); ?>">¿QUIÉNES
+						SOMOS?</a></li>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/portafolio'); ?>">PORTAFOLIO</a>
+				</li>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/trayectoria'); ?>">TRAYECTORIA</a>
+				</li>
+				<?php
+				if ($this->session->userdata("logged") == 1) {
+					echo '<li style="background-color: #ffffff5c;border-radius: 18px;" class="nav-item"><a class="nav-link" href="' . base_url('index.php/gerentesView') . '">SISTEMA DE GESTIÓN INTEGRAL</a></li>';
+				}
+				?>
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/noticiaseventos'); ?>">NOTICIAS
+						Y EVENTOS</a></li>
+				<!--<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/instalaciones') ?>">INSTALACIONES</a></li>-->
+				<li class="nav-item"><a class="nav-link" href="<?php echo base_url('index.php/contactenos') ?>">CONTÁCTENOS</a>
+				</li>
+				<?php
+
+				if ($this->session->userdata("logged") == 1) {
+					echo '<li class="nav-item">
+                                    <a class="p-1 nav-link btn  btn-sm text-uppercase" href="' . base_url('index.php/salir') . '"> Cerrar Sesión</a>
                                 </li>';
-                                echo '<li class="nav-item">
+					echo '<li class="nav-item">
                                     <a class="p-1 nav-link btn btnAcceder btn-xl text-uppercase" href="#"> 
                                         <div class="glyph-icon simple-icon-user"></div> 
-                                        Bienvenido:'.$this->session->userdata("User").'
+                                        Bienvenido:' . $this->session->userdata("User") . '
                                     </a>
-                                    </li>';                                
-                            }else{
-                                echo '<li class="nav-item "><a class="p-1 nav-link btn btnAcceder btn-danger btn-xl text-uppercase" href="'.base_url('index.php/login').'"> ACCEDER</a></li>';
-                            }
-                        ?>
+                                    </li>';
+				} else {
+					echo '<li class="nav-item "><a class="p-1 nav-link btn btnAcceder btn-danger btn-xl text-uppercase" href="' . base_url('index.php/login') . '"> ACCEDER</a></li>';
+				}
+				?>
 
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <section class="page-section" id="services">
-            <div class="container">
-                <div class="text-center">
-                    <h2 class="mt-5 section-heading text-uppercase" style="font-size: 2rem;">MISIÓN SGI</h2>
-                    
-                    <P class="text-start">Somos una Corporación 100% nicaragüense que se adhiere a los Principios Universales de Responsabilidad Social Empresarial, en el procesamiento, distribución y comercialización de Productos Cárnicos, Embutidos, Enlatados y Ahumados. </P>
-                </div>
-                <div class="text-center">
-                    <h2 class="mt-5 section-heading text-uppercase" style="font-size: 2rem;">POLÍTICA SGI</h2>
-                    
-                    <P class="text-start">Aplicamos la Prevención y el Pensamiento Basado en Riesgos y Oportunidades en todas nuestras actividades de procesos productivos, Distribución y Comercialización de nuestros productos. </P>
-                    <P class="text-start">Cumplimos los requisitos legales y reglamentarios aplicables, en lo relacionado con Calidad, Inocuidad, Seguridad y Salud en el Trabajo, y Asuntos Ambientales.</P>
-                    <P class="text-start">Fijamos como premisa institucional para el éxito sostenible, la mejora continua en el desempeño de nuestros procesos, productos y Sistema de Gestión Integral, para contribuir en el desarrollo socioeconómico del país, la seguridad alimentaria, el bienestar de nuestros colaboradores y sus familias. </P>
-                </div>
-            </div>
-        </section>
-     
-        
-        <!-- Footer-->
-        <footer class="footer py-4">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-4 text-lg-start">Copyright &copy; DELMOR S.A <?php echo date('Y'); ?></div>
-                    <div class="col-lg-4 my-3 my-lg-0">
-                        <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/delmornic/?hl=es-la" aria-label="Twitter"><i class="fab fa-instagram"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/delmornic/" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="https://www.linkedin.com/company/industrias-delmor-s-a/?originalSubdomain=ni" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                    <div class="col-lg-4 text-lg-end">
-                        
-                    </div>
-                </div>
-            </div>
-        </footer>       
-        <!-- Bootstrap core JS-->
-        <script src="<?php echo base_url()?>assets/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="<?php echo base_url()?>assets/js/scriptslanding.js"></script>
-        <!--<script src="<?php echo base_url()?>assets/js/sb-forms-latest.js"></script>-->
-    </body>
+			</ul>
+		</div>
+	</div>
+</nav>
+<section class="page-section" id="services">
+	<div class="container">
+		<div class="text-center">
+			<h2 class="mt-5 section-heading text-uppercase" style="font-size: 2rem;">MISIÓN SGI</h2>
+
+			<P class="text-start">Somos una Corporación 100% nicaragüense que se adhiere a los Principios Universales de
+				Responsabilidad Social Empresarial, en el procesamiento, distribución y comercialización de Productos
+				Cárnicos, Embutidos, Enlatados y Ahumados. </P>
+		</div>
+		<div class="text-center">
+			<h2 class="mt-5 section-heading text-uppercase" style="font-size: 2rem;">POLÍTICA SGI</h2>
+
+			<P class="text-start">Los principios de prevención, pensamiento basado en riesgos y oportunidades,
+				garantizan la inocuidad en las actividades de producción, distribución y comercialización de productos
+				cárnicos, embutidos y ahumados.
+				Cumplimos los requisitos legales y reglamentarios aplicables, en lo relacionado con Calidad, Inocuidad,
+				Seguridad y Salud en el Trabajo, así como en Asuntos Ambientales.
+				Para el éxito sostenible fijamos como premisa institucional, la mejora continua en el desempeño del
+				Sistema de Gestión Integral, fortaleciendo las competencias de los colaboradores, a fin de garantizar la
+				seguridad alimentaria, contribuir al desarrollo socioeconómico del país y al bienestar de nuestra
+				comunidad y sus familias.
+				Promovemos canales eficaces de comunicación, tanto internos como externos, que aseguren la comprensión y
+				aplicación de esta política a todos los niveles de la organización.
+			</P>
+		</div>
+	</div>
+</section>
+
+
+<!-- Footer-->
+<footer class="footer py-4">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-4 text-lg-start">Copyright &copy; DELMOR S.A <?php echo date('Y'); ?></div>
+			<div class="col-lg-4 my-3 my-lg-0">
+				<a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/delmornic/?hl=es-la"
+				   aria-label="Twitter"><i class="fab fa-instagram"></i></a>
+				<a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/delmornic/"
+				   aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+				<a class="btn btn-dark btn-social mx-2"
+				   href="https://www.linkedin.com/company/industrias-delmor-s-a/?originalSubdomain=ni"
+				   aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<div class="col-lg-4 text-lg-end">
+
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="<?php echo base_url() ?>assets/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="<?php echo base_url() ?>assets/js/scriptslanding.js"></script>
+<!--<script src="<?php echo base_url() ?>assets/js/sb-forms-latest.js"></script>-->
+</body>
 </html>
